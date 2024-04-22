a306d6460a011c128a88242296e8340d7127fcb0
Tobias Hahnen <tobias.hahnen@sonarsource.com>
2024-04-22 22:42:47 +0200

    Script for creating dogfood IDE
    Based on the latest integration build and the (possible) dogfood version of
    plug-in / features. Additionally, add public information to the README.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# eclipse-ide-tools
-Various tooling for working with the Eclipse IDE
+# Tooling around the Eclipse IDE
+
+This repository contains general tooling around the Eclipse IDE such as scripts for building a
+specific environment or plug-ins bringing quality of life features or patches.
+
+## Projects
+
+This paragraph provides a quick abstract of all the projects inside this repository as content such
+as scripts or plug-ins are grouped together or located in their destinct folder.
+
+#### iBuilds IDE
+
+This creates a destinct Eclipse IDE installation based on the latest (stable) integration build of
+the Eclipse SDK with all the necessary features / plug-ins / configurations used for development in
+order to dogfood all the components while having a somewhat stable environment for actual proper
+development and not just for messing around.
+
+TODOs:
+- add configuration application / plug-in for setting up workspace with specific preferences
+- not only replace same artifact but get rid of all old iBuild applications / workspaces
+- back-up workspace in case something breaks
+- forbid users to create projects inside the workspace, just outside of it
+
+## Knowledge base
+
+This paragraph contains some information for working with the Eclipse IDE in order to access some
+information not well known or not publically available.
+
+#### List all available Eclipse Update Sites (after opened at least once) on macOS
+> cat $APPLICATION/Contents/Eclipse/p2/org.eclipse.equinox.p2.engine/profileRegistry/$PROFILE/.data/.settings/org.eclipse.equinox.p2.artifact.repository.prefs
+
+The actual URIs are available when running the command piped with `grep "/uri="`.
---
+++ prj-iBuilds-IDE/build.sh
@@ -0,0 +1,162 @@
+#!/usr/bin/env bash
+
+# Eclipse installation from latest stable integration build
+# =========================================================
+
+DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+BUILD_DIR="$DIR/build"
+COMPOSITE_ARTIFACTS_JAR_FILE="$BUILD_DIR/compositeArtifacts.jar"
+COMPOSITE_ARTIFACTS_XML_FILE="$BUILD_DIR/compositeArtifacts.xml"
+INSTALLER_FILE="$BUILD_DIR/installer.dmg"
+
+# Replace template with value in a file
+function replaceStringInFile() {
+    sed -ir "s#$2#$3#" $1
+    rm "$1r"
+}
+
+# Await user input until "yes" is answered 
+function awaitUser() {
+    while true; do
+        read "yn?$1 "
+        case $yn in
+            [Yy]* ) break;;
+            *) echo "Please answer with yes!";;
+        esac
+    done
+}
+
+
+# =============================================================================
+#   *) Templated and fallback configuration
+# =============================================================================
+ECLIPSE_VERSION="4.32"
+ECLIPSE_COMPOSITE_URL="https://www.eclipse.org/downloads/download.php?file=/eclipse/updates/$ECLIPSE_VERSION-I-builds/compositeArtifacts.jar"
+ECLIPSE_DMG_TEMPLATE="https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops4/VERSION/eclipse-SDK-VERSION-macosx-cocoa-ARCH.dmg"
+
+ECLIPSE_ARCH="$(uname -m)"
+if [[ "$ECLIPSE_ARCH" == "arm64" ]]; then
+    ECLIPSE_ARCH="aarch64"
+fi
+
+
+# =============================================================================
+#   1) Remove old artifacts
+# =============================================================================
+rm -rf $BUILD_DIR >/dev/null 2>&1
+
+
+# =============================================================================
+#   2) Download compositeArtifacts.jar and extract XML for newest version
+# =============================================================================
+mkdir $BUILD_DIR >/dev/null 2>&1
+wget $ECLIPSE_COMPOSITE_URL -O $COMPOSITE_ARTIFACTS_JAR_FILE >/dev/null 2>&1
+unzip $COMPOSITE_ARTIFACTS_JAR_FILE -d $BUILD_DIR >/dev/null 2>&1
+rm -f $COMPOSITE_ARTIFACTS_JAR_FILE >/dev/null 2>&1
+
+I_BUILDS_VERSION="$(xmllint --xpath 'string(/repository/children/child[last()]/@location)' $COMPOSITE_ARTIFACTS_XML_FILE)"
+
+
+# =============================================================================
+#   2) Download installer and unzip the actual installation
+# =============================================================================
+ECLIPSE_URL="${ECLIPSE_DMG_TEMPLATE//VERSION/$I_BUILDS_VERSION}"
+ECLIPSE_URL="${ECLIPSE_URL//ARCH/$ECLIPSE_ARCH}"
+
+wget $ECLIPSE_URL -O $INSTALLER_FILE >/dev/null 2>&1
+7zz -o$BUILD_DIR x $INSTALLER_FILE >/dev/null 2>&1
+rm -f $INSTALLER_FILE >/dev/null 2>&1
+
+
+# =============================================================================
+#   3) Prepare iBuilds installation for additional changes
+# =============================================================================
+APPLICATION_NAME="Eclipse-$I_BUILDS_VERSION.app"
+APPLICATION_FILE="$BUILD_DIR/$APPLICATION_NAME"
+
+mv $BUILD_DIR/Eclipse/Eclipse.app $APPLICATION_FILE >/dev/null 2>&1
+rm -rf $BUILD_DIR/Eclipse >/dev/null 2>&1
+
+
+# =============================================================================
+#   4) Fix configuration: config.ini with default workspace
+# =============================================================================
+CONFIG_DIR="$APPLICATION_FILE/Contents/Eclipse/configuration"
+
+replaceStringInFile "$CONFIG_DIR/config.ini" "@user.home/Documents/workspace" \
+    "@user.home/workspaces/$I_BUILDS_VERSION"
+
+
+# =============================================================================
+#   5) Fix installation: Info.plist with identifier / (display) name
+# =============================================================================
+INFO_PLIST="$APPLICATION_FILE/Contents/Info.plist"
+
+replaceStringInFile $INFO_PLIST "<string>org.eclipse.sdk.ide</string>" \
+    "<string>org.eclipse.sdk.ide.$I_BUILDS_VERSION</string>"
+replaceStringInFile $INFO_PLIST "<string>Eclipse</string>" \
+    "<string>Eclipse $I_BUILDS_VERSION</string>"
+
+
+# =============================================================================
+#   6) Install necessary plug-ins for development
+# =============================================================================
+touch "$APPLICATION_FILE" >/dev/null 2>&1
+codesign --force --deep --sign - "$APPLICATION_FILE" >/dev/null 2>&1
+
+$APPLICATION_FILE/Contents/MacOS/eclipse -noSplash \
+    -application org.eclipse.equinox.p2.director \
+    -repository https://download.eclipse.org/technology/m2e/snapshots/latest/ \
+    -installIU org.eclipse.m2e.sdk.feature.feature.group \
+    -profile SDKProfile \
+    -followReferences
+
+$APPLICATION_FILE/Contents/MacOS/eclipse -noSplash \
+    -application org.eclipse.equinox.p2.director \
+    -repository https://binaries.sonarsource.com/SonarLint-for-Eclipse/dogfood/ \
+    -installIU org.sonarlint.eclipse.feature.feature.group \
+    -profile SDKProfile \
+    -followReferences
+
+$APPLICATION_FILE/Contents/MacOS/eclipse -noSplash \
+    -application org.eclipse.equinox.p2.director \
+    -repository https://download.eclipse.org/reddeer/releases/latest/ \
+    -installIU org.eclipse.reddeer.eclipse.feature.feature.group,\
+org.eclipse.reddeer.logparser.feature.feature.group,\
+org.eclipse.reddeer.recorder.feature.feature.group,\
+org.eclipse.reddeer.spy.feature.feature.group,\
+org.eclipse.reddeer.swt.feature.feature.group,\
+org.eclipse.reddeer.ui.feature.feature.group \
+    -profile SDKProfile \
+    -followReferences
+
+
+# =============================================================================
+#   7) Remove build script and sign again
+# =============================================================================
+pushd $CONFIG_DIR
+rm *.log >/dev/null 2>&1
+popd
+
+touch "$APPLICATION_FILE" >/dev/null 2>&1
+codesign --force --deep --sign - "$APPLICATION_FILE" >/dev/null 2>&1
+
+
+# =============================================================================
+#   8) Move to user application folder and replace existing installation
+# =============================================================================
+INSTALLATION_DIR="$HOME/Applications/$APPLICATION_NAME"
+
+if [[ -f "$INSTALLATION_DIR" ]]; then
+    rm -rf $INSTALLATION_DIR >/dev/null 2>&1
+    which dockutil >/dev/null 2>&1
+    if [[ "$?" -eq "0" ]]; then
+        dockutil --remove "org.eclipse.sdk.ide.$I_BUILDS_VERSION"
+    fi
+fi
+
+mv $APPLICATION_FILE $INSTALLATION_DIR >/dev/null 2>&1
+which dockutil >/dev/null 2>&1
+if [[ "$?" -eq "0" ]]; then
+    dockutil --add $INSTALLATION_DIR >/dev/null 2>&1
+fi
